811f232ec1e5e71069d36a2a8b8fceeae76f065b
Sagar-devx <sagarsharma.devx@gmail.com>
2026-07-06 12:14:51 +0530

    docs: add swagger schema documentation for planType in OrderRequest

--- src/main/java/com/sagar/resume/dto/OrderRequest.java
+++ src/main/java/com/sagar/resume/dto/OrderRequest.java
@@ -1,5 +1,6 @@
 package com.sagar.resume.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class OrderRequest {
 
     @NotBlank(message = "Plan type is required")
+    @Schema(description = "Type of subscription plan (Only 'PREMIUM' is currently supported)", example = "PREMIUM", allowableValues = {"PREMIUM"})
     private String planType;
 
     public String getPlanType() {
